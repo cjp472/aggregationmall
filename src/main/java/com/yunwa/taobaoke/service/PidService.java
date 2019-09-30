326421256ee52db4db4adecbc8878884d0de5a23
coderyang123 <1023178796@qq.com>
2019-09-30 15:16:49 +0800

    9.30 提交

--- src/main/java/com/yunwa/taobaoke/service/PidService.java
+++ src/main/java/com/yunwa/taobaoke/service/PidService.java
@@ -2,6 +2,6 @@ package com.yunwa.taobaoke.service;
 
 import java.util.List;
 
-public interface AdminService {
+public interface PidService {
     void getPid(long number, List<String> p_id_name_list);
 }
